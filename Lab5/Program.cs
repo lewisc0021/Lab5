4eb8d2ad98b4809033fed941d76e5e529aa85742
Tommy Lewis <lewisc0021@gmail.com>
2016-11-04 16:54:45 -0400

    first commit

---
+++ Lab5/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("How many sides do these die have?");
+                int sides;
+                int dice1;
+                int dice2;
+                while (!int.TryParse(Console.ReadLine(), out sides))
+                {
+                    Console.WriteLine("Please input a correct value.");
+                }
+                rollDice(sides, out dice1, out dice2);
+                Console.WriteLine($"Your first roll was: {dice1}, Your second roll was: {dice2}");
+                specialMessage(dice1, dice2);
+                Console.WriteLine("Enter y or Y if you want to continue.");
+                string answer = Console.ReadLine();
+                if (!(answer == "Y" || answer == "y"))
+                    break;
+            }
+        }
+        static void rollDice( int sides, out int roll1, out int roll2)
+        {   
+            Random r1 = new Random(DateTime.Now.Millisecond);
+            roll1 = r1.Next(1, sides);
+            roll2 = r1.Next(1, sides);
+        }
+        static void specialMessage(int roll1, int roll2)
+        {
+            if (roll1 == 6 && roll2 == 6)
+            {
+                Console.WriteLine("Boxcars!");
+            }
+            else if (roll1 == 1 && roll2 == 1)
+            {
+                Console.WriteLine("Snake Eyes!");
+            }
+            else if ((roll1 == 7 && roll2 == 11) || (roll2 == 7 && roll2 == 11))
+            {
+                Console.WriteLine("Craps!");
+            }
+        }
+    }
+}
